364a8b1bcddf0535f0bc27204637104619bb1c37
cheilamelo <cheillaa@hotmail.com>
2023-08-11 15:20:35 -0300

    app/admin movido para app/(admin)/[[...index]] e copiar layout.tsx para esse diretório

---
+++ app/(admin)/admin/[[...index]]/layout.tsx
@@ -0,0 +1,16 @@
+//import './globals.css'
+
+import Head from "./head";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt">
+      <Head />
+      <body>{children}</body>
+    </html>
+  );
+}
